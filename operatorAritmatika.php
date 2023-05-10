0cac32f41fafbcfd0b6bdde21d18743f18651a68
simonsaputra <simon@smkwikrama.sch.id>
2023-05-10 08:58:22 +0700

    ngulang

--- operatorAritmatika.php
+++ operatorAritmatika.php
@@ -4,31 +4,43 @@ $a = 30;
 $b = 2;
 
 //Penjumlahan
+echo "penjumlahan";
+echo "<br>";
 $c = $a + $b;
 echo "$a + $b = $c";
 echo "<hr>";
 
 //Pengurangan
+echo "Pengurangan";
+echo "<br>";
 $c = $a - $b;
 echo "$a - $b = $c";
 echo "<hr>";
 
 //Perkalian
+echo "Perkalian";
+echo "<br>";
 $c = $a * $b;
 echo "$a * $b = $c";
 echo "<hr>";
 
 //Pembagian
+echo "Pembagian";
+echo "<br>";
 $c = $a / $b;
 echo "$a / $b = $c";
 echo "<hr>";
 
 //Sisa bagi / modulus 
+echo "Modulus";
+echo "<br>";
 $c = $a % $b;
 echo "$a % $b = $c";
 echo "<hr>";
 
 //Pangkat
+echo "Perpangkatan";
+echo "<br>";
 $c = $a ** $b;
 echo "$a ** $b = $c";
 echo "<hr>";
