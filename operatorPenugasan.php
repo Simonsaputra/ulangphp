9f47fe50fc5e658f45c57ff6723f97da970809ae
simonsaputra <simon@smkwikrama.sch.id>
2023-05-10 13:30:51 +0700

    tugas

---
+++ operatorPenugasan.php
@@ -0,0 +1,16 @@
+<?php 
+echo "penjumlahan / increment";
+echo "<br>";
+$x = 2;
+$x++;
+echo $x;
+echo "<br>";
+
+echo "pengurangan / decrement";
+echo "<br>";
+$a = 100;
+$a--;
+echo $a;
+
+
+?>
---
+++ tugasperbandingan.php
@@ -0,0 +1,29 @@
+<?php
+    
+    echo var_dump(10<15); 
+    echo "<br />";
+    echo "<hr>";
+             
+    echo var_dump(12<12); 
+    echo "<br />";
+    echo "<hr>";
+             
+    echo var_dump(13<=13); 
+    echo "<br />";
+    echo "<hr>";
+             
+    echo  var_dump(10<>'10');  
+    echo "<br />";
+    echo "<hr>";
+             
+    echo  var_dump(9=='9'); 
+    echo "<br />";
+    echo "<hr>";
+             
+    echo  var_dump(10==='10'); 
+    echo "<br />";
+    echo "<hr>";
+             
+    echo  var_dump('150'=='1.5e2'); 
+    echo"<br/>";
+?>
